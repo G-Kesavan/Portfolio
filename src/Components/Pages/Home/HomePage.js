911c4a78fc12451752710aa0f73dcdc1eeaae4e5
G-Kesavan <mr.kesavaperumal@gmail.com>
2025-04-21 19:52:26 +0530

    add

--- src/Components/Pages/Home/HomePage.js
+++ src/Components/Pages/Home/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './HomePage.css'
 import Photo from '../../../Image/MyPhoto.png'
+import { CircularProgressBar } from "react-percentage-bar";
 
 const HomePage = () => {
   return (
@@ -22,7 +23,30 @@ const HomePage = () => {
       </div>
       
       <div className='language'>
-        <div>language</div>
+        <div className='languages'>
+          <p>English</p>
+          <div className='bar'>
+            <CircularProgressBar
+            key=""
+            styles={{root:{
+              width:'80px',
+              height:'80px',
+            },}}
+            percentage='65'
+            color="#c6c1c1bd"
+            trackColor='#3e3a3abe'
+          />
+          </div>
+        </div>
+        <div className='languages'>
+          <p>Tamil</p>
+          <CircularProgressBar
+            key="" 
+            percentage='65'
+            color="#c6c1c1bd"
+            trackColor='#3e3a3abe'
+          />
+        </div>      
       </div>
     </section>
   )
