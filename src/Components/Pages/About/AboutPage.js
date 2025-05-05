e1781c2190c3a51867d1644b1f9c72cf81367562
GOOD WELL <mr.kesavaperumal@gmail.com>
2025-05-05 14:38:03 +0530

    add: some page

--- src/Components/Pages/About/AboutPage.js
+++ src/Components/Pages/About/AboutPage.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import './About.css'
+import Photo from '../../../Image/MyPhoto.png'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='AboutPage'>
+      <div className='part1'>
+        <img src={Photo} alt='profile'/>
+        <div className='detial'>
+          <p>name</p>
+          <p>name</p>
+          <p>name</p>
+          <p>name</p>
+        </div>
+
+        <div className='point'>
+        <p>name</p>
+          <p>name</p>
+          <p>name</p>
+          <p>name</p>
+        </div>
+
+      </div>
+
+      <div className='part2'>
+        <div className='education'>
+          ed
+        </div>
+
+        <div className='basic'>
+          weakness
+        </div>
+        
+      </div>
+    </section>
   )
 }
 
--- src/Components/Pages/Contact/ContactPage.js
+++ src/Components/Pages/Contact/ContactPage.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import './Contact.css'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section className='ContactPage'>
+      <form className='contactForm'>
+        <table>
+          <tr>
+            <td><label>Name :</label></td><td><input type='text' required></input></td>
+          </tr>
+          <tr>
+            <td><label>From :</label></td><td><input type='mail' required></input></td>
+          </tr>
+          <tr>
+            <td><input type='adderss' resource=''></input></td>
+          </tr>
+          <tr></tr>
+        </table>
+        <button>Submit</button>
+      </form>
+      <div className='link'>
+        <ul>
+          <li>whatapp</li>
+          <li>whatapp</li>
+          <li>whatapp</li>
+          <li>whatapp</li>
+        </ul>
+      </div>
+    </section>
   )
 }
 
