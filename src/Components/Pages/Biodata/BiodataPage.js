a0743eb4a16eefee0d54f31513e3d1cf23172801
G-Kesavan <mr.kesavaperumal@gmail.com>
2025-06-02 03:29:54 -0700

    some change

--- src/Components/Pages/Biodata/BiodataPage.js
+++ src/Components/Pages/Biodata/BiodataPage.js
@@ -5,31 +5,37 @@ import Photo from '../../../Image/MyPhoto.png'
 const Biodata = () => {
   return (
     <section className='BiodataPage'>
- <div className='part1'>
-        <img src={Photo} alt='profile'/>
+      <div className='part1'>
+        <image>
+          <img src={Photo} alt='profile'/>
+        </image>
         <div className='detial'>
-          <p>name</p>
-          <p>name</p>
-          <p>name</p>
-          <p>name</p>
+          <p><label>Name : </label>Kesava perumal g</p>
+          <p><label>Age : </label>22</p>
+          <p><label>Qulification : </label>BCA</p>
+          <p><label>Batch : </label>2025</p>
         </div>
 
         <div className='point'>
-        <p>name</p>
-          <p>name</p>
-          <p>name</p>
-          <p>name</p>
+          <p><label>Date of birth:</label>09/03/2003</p>
+          <p><label>Gender:</label>Male</p>
+          <p><label>Religen:</label>india</p>
+          <p><label>State:</label>Tamil nadu</p>
+          <p><label>Mother tang:</label>Tamil</p>
         </div>
 
       </div>
 
       <div className='part2'>
-        <div className='education'>
-          ed
-        </div>
+        <ul className='education'>
+          <li>10th</li>
+          <li>12th</li>
+          <li>apprenticeship</li>
+          <li>college</li>
+        </ul>
 
         <div className='basic'>
-          weakness
+          
         </div>
         
       </div>
