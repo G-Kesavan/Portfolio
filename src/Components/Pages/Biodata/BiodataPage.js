b3ff094720d30cd7b448d7b46bc008f4f28e0718
GOOD WELL <mr.kesavaperumal@gmail.com>
2025-06-04 17:37:59 +0530

    add biodata

--- src/Components/Pages/Biodata/BiodataPage.js
+++ src/Components/Pages/Biodata/BiodataPage.js
@@ -1,73 +1,71 @@
-import React from 'react'
-import './Biodata.css'
-import Photo from '../../../Image/MyPhoto.png'
+import React from 'react';
+import './Biodata.css';
+import Photo from '../../../Image/MyPhoto.png';
 
 const Biodata = () => {
   return (
     <section className='BiodataPage'>
       <div className='part1'>
-        <div>
-          <img src={Photo} alt='profile'/>
+        <div className='image'>
+          <img src={Photo} alt='Profile' />
         </div>
+
         <div className='detial'>
-          <p><label>Name : </label>Kesava perumal g</p>
-          <p><label>Age : </label>22</p>
-          <p><label>Qulification : </label>BCA</p>
-          <p><label>Batch : </label>2025</p>
+          <p><label>Name</label>: Kesava Perumal G</p>
+          <p><label>Age</label>: 22</p>
+          <p><label>DOB</label>: 09/03/2003</p>
+          <p><label>Gender</label>: Male</p>
+          <p><label>Quals</label>: BCA</p>
+          <p><label>Batch</label>: 2025</p>
         </div>
 
-        <div className='point'>
-          <p><label>Date of birth:</label>09/03/2003</p>
-          <p><label>Gender:</label>Male</p>
-          <p><label>Religion:</label>india</p>
-          <p><label>State:</label>Tamil nadu</p>
-          <p><label>Mother tang:</label>Tamil</p>
-        </div>
-
+        <ul className='point'>
+          <li><label>Address :</label> <p>8/277, Velikunan Kurichi, Virudhachalam TK, Cuddalore, Tamil Nadu - 607804</p></li>
+          <li><label>Marital Status</label>: <p>Single</p></li>
+          <li><label>Nation</label>: <p>India</p></li>
+          <li><label>State</label>: <p>Tamil Nadu</p></li>
+          <li><label>(MT)</label>: <p>Tamil</p></li>
+        </ul>
       </div>
 
       <div className='part2'>
-        <ul className='edu'>
+        <ul className='education-ul'>
           <li>
             <div className='edu-detial'>
               <h3>SSLC</h3>
-              <h4>State bourd of Tamilnadu</h4>
-              <p>NLC Her Sec Re School mandarakuppam</p>
+              <h4>State Board of Tamil Nadu</h4>
+              <p>(NLC Her Sec Re School, Mandarakuppam)</p>
               <p>2017 to 2018</p>
             </div>
           </li>
           <li>
             <div className='edu-detial'>
               <h3>HSS</h3>
-              <h4>State bourd of Tamilnadu</h4>
-              <p>NLC Her Sec Re School mandarakuppam</p>
+              <h4>State Board of Tamil Nadu</h4>
+              <p>(NLC Her Sec Re School, Mandarakuppam)</p>
               <p>2018 to 2020</p>
             </div>
           </li>
-          <li><div className='edu-detial'>
-              <h3>Appretice Ship</h3>
-              <h4>Minastry of skill development</h4>
-              <p>NLC (national lignight corpretion)</p>
+          <li>
+            <div className='edu-detial'>
+              <h3>Apprenticeship</h3>
+              <h4>Ministry of Skill Development</h4>
+              <p>NLC (Neyveli Lignite Corporation)</p>
               <p>2020 to 2022</p>
             </div>
           </li>
           <li>
             <div className='edu-detial'>
               <h3>BCA</h3>
-              <h4>Annamalai Universty</h4>
-              <p>Jawaher Science College Neyveli-2</p>
+              <h4>Annamalai University</h4>
+              <p>(Jawaher Science College, Neyveli-2)</p>
               <p>2022 to 2025</p>
             </div>
           </li>
         </ul>
-
-        <div className='basic'>
-          
-        </div>
-        
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Biodata
+export default Biodata;
