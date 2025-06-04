de189dedda78179818d7b1bae045e84631405dd4
G-Kesavan <mr.kesavaperumal@gmail.com>
2025-06-03 23:10:39 -0700

    some change

--- src/Components/Pages/Biodata/BiodataPage.js
+++ src/Components/Pages/Biodata/BiodataPage.js
@@ -6,9 +6,9 @@ const Biodata = () => {
   return (
     <section className='BiodataPage'>
       <div className='part1'>
-        <image>
+        <div>
           <img src={Photo} alt='profile'/>
-        </image>
+        </div>
         <div className='detial'>
           <p><label>Name : </label>Kesava perumal g</p>
           <p><label>Age : </label>22</p>
@@ -19,7 +19,7 @@ const Biodata = () => {
         <div className='point'>
           <p><label>Date of birth:</label>09/03/2003</p>
           <p><label>Gender:</label>Male</p>
-          <p><label>Religen:</label>india</p>
+          <p><label>Religion:</label>india</p>
           <p><label>State:</label>Tamil nadu</p>
           <p><label>Mother tang:</label>Tamil</p>
         </div>
@@ -27,11 +27,38 @@ const Biodata = () => {
       </div>
 
       <div className='part2'>
-        <ul className='education'>
-          <li>10th</li>
-          <li>12th</li>
-          <li>apprenticeship</li>
-          <li>college</li>
+        <ul className='edu'>
+          <li>
+            <div className='edu-detial'>
+              <h3>SSLC</h3>
+              <h4>State bourd of Tamilnadu</h4>
+              <p>NLC Her Sec Re School mandarakuppam</p>
+              <p>2017 to 2018</p>
+            </div>
+          </li>
+          <li>
+            <div className='edu-detial'>
+              <h3>HSS</h3>
+              <h4>State bourd of Tamilnadu</h4>
+              <p>NLC Her Sec Re School mandarakuppam</p>
+              <p>2018 to 2020</p>
+            </div>
+          </li>
+          <li><div className='edu-detial'>
+              <h3>Appretice Ship</h3>
+              <h4>Minastry of skill development</h4>
+              <p>NLC (national lignight corpretion)</p>
+              <p>2020 to 2022</p>
+            </div>
+          </li>
+          <li>
+            <div className='edu-detial'>
+              <h3>BCA</h3>
+              <h4>Annamalai Universty</h4>
+              <p>Jawaher Science College Neyveli-2</p>
+              <p>2022 to 2025</p>
+            </div>
+          </li>
         </ul>
 
         <div className='basic'>
